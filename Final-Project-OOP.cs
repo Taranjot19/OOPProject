aa885afecbc41202f6ae7e60cd4d21abea79251c
Mehak820 <mehakbrar343@gmail.com>
2025-04-14 14:52:39 -0600

    Add files via upload

---
+++ Final-Project-OOP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    internal class Program
+    {
+        static char[,] board = new char[6, 7];
+        static char currentPlayer = 'X';
+            static void Main(string[] args)
+        {
+        }
+    }
+}
